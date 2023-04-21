76664ea0f0ac38de4a4668507e06ca3607f12a1c
kuriyantoadi <melatikuning212@gmail.com>
2023-04-22 00:48:16 +0700

    update captcha

---
+++ application/controllers/Komentar.php
@@ -0,0 +1,52 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Komentar extends CI_Controller {
+
+	public function index()
+	{
+		$config = [
+			"img_path" => "./captcha/",
+			"img_url" => base_url('captcha'),
+			"img_width" => 160,
+			"img_height" => 30,
+			"border" => 4,
+			"expiration" => 30,
+			'word_length'   => 4,
+			'font_size'     => 30,
+			'pool'          => '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+
+
+			'colors'        => array(
+                'background' => array(255, 255, 255),
+                'border' => array(255, 255, 255),
+                'text' => array(0, 0, 0),
+				'grid' => array(255, 40, 40)
+			)
+
+		];
+
+		
+
+		$captcha = create_captcha($config);
+
+		$this->session->set_userdata('captcha_word', $captcha['word']);
+
+		$data["image"] = $captcha['image'];
+
+		$this->load->view('komentar/index', $data);
+	}
+
+	public function simpanKomentar()
+	{
+		$currentCaptcha = $this->session->userdata('captcha_word');
+		$sendedCaptcha = $this->input->post('captcha');
+
+		if($currentCaptcha = $sendedCaptcha) {
+			echo "captcha benar";
+		}else{
+			echo "captcha salah";
+		}
+
+	}
+}
---
+++ application/views/komentar/index.php
@@ -0,0 +1,30 @@
+﻿
+<!DOCTYPE html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>Komentar</title>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+
+<body>
+
+</head>
+<div style="margin-left: 500px; margin-top: 100px;">
+<form action="<?= site_url('Komentar/simpanKomentar') ?>" method="POST">
+<label>Name</label>
+<br>
+<input type="text" name="name">
+<br><br>
+<label>Komentar</label>
+<br>
+<textarea name="komentar" cols="30" rows="10"></textarea> <br><br>
+<br><?= $image; ?>
+
+<br><label for="">Captcha</label>
+
+<br><input type="text" name="captcha">
+<br>
+<br><button type="submit">Submit</button>
+</form>
+</div>
+</body>
+ </html>
